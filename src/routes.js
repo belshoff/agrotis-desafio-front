9c71a6bf078ab70f2ce19059554d6445d25723ac
William Vitorino <williamvitorino3@gmail.com>
2020-03-12 19:05:16 -0300

    baseurl

--- src/routes.js
+++ src/routes.js
@@ -5,13 +5,13 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Home from './pages/home'
 import Form from './pages/form'
 
-const baseUrl = '/'
+const baseUrl = '/agrotis-desafio-front'
 
 const Routes = () => (
-  <BrowserRouter>
+  <BrowserRouter basename={baseUrl} >
     <Switch>
-      <Route exact path={baseUrl} component={Home} />
-      <Route path={baseUrl+"form"} component={Form} />
+      <Route exact path={'/'} component={Home} />
+      <Route path={"/form"} component={Form} />
     </Switch>
   </BrowserRouter>
 )
