d9c122dd89f9e60559e60a5322eac23689e2fb31
William Vitorino <williamvitorino3@gmail.com>
2020-03-12 19:06:30 -0300

    9c71a6b

---
+++ precache-manifest.05c80d5df28776a01003f48788d4e9a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11cb7c8bf1d365f807b8bddc16106bc0",
+    "url": "/agrotis-desafio-front/index.html"
+  },
+  {
+    "revision": "a7f1ca15392777f0324c",
+    "url": "/agrotis-desafio-front/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1e3aa29841aba95e5c59",
+    "url": "/agrotis-desafio-front/static/css/main.73833617.chunk.css"
+  },
+  {
+    "revision": "a7f1ca15392777f0324c",
+    "url": "/agrotis-desafio-front/static/js/2.97da5c96.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/agrotis-desafio-front/static/js/2.97da5c96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e3aa29841aba95e5c59",
+    "url": "/agrotis-desafio-front/static/js/main.6468f136.chunk.js"
+  },
+  {
+    "revision": "e0726293b4c36ef6c86e",
+    "url": "/agrotis-desafio-front/static/js/runtime-main.3df2b7c5.js"
+  },
+  {
+    "revision": "2f7f9a6b7cab2503f60331e6d9a47837",
+    "url": "/agrotis-desafio-front/static/media/find.2f7f9a6b.svg"
+  }
+]);
